b683ea09967926febbf50d90a56f0c6b020e79d5
xelio-plus <xelio-plus@outlook.com>
2014-12-29 20:25:15 +0100

    Customize logo and icons

--- tpl_header.php
+++ tpl_header.php
@@ -13,10 +13,6 @@ if (!defined('DOKU_INC')) die();
     <?php tpl_includeFile('header.html') ?>
 
     <div class="headings group">
-        <ul class="a11y skip">
-            <li><a href="#dokuwiki__content"><?php echo $lang['skip_to_content']; ?></a></li>
-        </ul>
-
         <h1><?php
             // get logo either out of the template images folder or data/media folder
             $logoSize = array();
@@ -25,7 +21,7 @@ if (!defined('DOKU_INC')) die();
             // display logo and wiki title in a link to the home page
             tpl_link(
                 wl(),
-                '<img src="'.$logo.'" '.$logoSize[3].' alt="" /> <span>'.$conf['title'].'</span>',
+                '<img src="'.$logo.'" '.$logoSize[3].' alt="'.$conf['title'].'" />',
                 'accesskey="h" title="[H]"'
             );
         ?></h1>
@@ -46,10 +42,14 @@ if (!defined('DOKU_INC')) die();
                             tpl_userinfo(); /* 'Logged in as ...' */
                             echo '</li>';
                         }
-                        tpl_action('admin', 1, 'li');
-                        tpl_action('profile', 1, 'li');
                         tpl_action('register', 1, 'li');
                         tpl_action('login', 1, 'li');
+                    ?>
+                </ul>
+                <ul>
+                    <?php
+                        tpl_action('admin', 1, 'li');
+                        tpl_action('profile', 1, 'li');
                     ?>
                 </ul>
             </div>
